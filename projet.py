a01f35feb74472907920af33f091e83f0e81ed47
Malware Pink Panda <37533238+AxelSliva@users.noreply.github.com>
2020-11-11 17:51:04 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -7,24 +7,18 @@ from datetime import datetime
 import requests
 import time
 
-#url = 'http://vxvault.net/ViriList.php'
 url = 'https://www.exploit-db.com/'
 
 browser = webdriver.Firefox()
 browser.get(url)
-
-
 time.sleep(5)
-
-
-
 element = browser.find_elements_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div/div[2]/div/table/thead/tr/th')
 elements =browser.find_elements_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr/td')
-#elements = browser.find_elements_by_class_name('text-center')
 
 len(element)
 len(elements)
 
-
 for e in elements :
     print(e.text)
+
+print('END')
