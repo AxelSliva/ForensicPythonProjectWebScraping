b40595c9c92bc9060ab2ecfd8ad5f415d5bb5125
Malware Pink Panda <37533238+AxelSliva@users.noreply.github.com>
2020-11-11 18:43:09 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -13,7 +13,7 @@ browser = webdriver.Firefox()
 browser.get(url)
 time.sleep(5)
 element = browser.find_elements_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div/div[2]/div/table/thead/tr/th')
-elements =browser.find_elements_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr/td')
+elements = browser.find_elements_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr/td')
 
 len(element)
 len(elements)
@@ -21,4 +21,20 @@ len(elements)
 for e in elements :
     print(e.text)
 
-print('END')
+res=[]
+for i in range ( 0, len(elements),8):
+    Date = elements[i].text
+    D = elements[i +1].text
+    A = elements[i +2].text
+    V = elements[i +3].text
+    Title = elements[i +4].text
+    Type = elements[i +5].text
+    Platform = elements[i +6].text
+    Author = elements[i +7].text
+    res.append((Date,D,A,V,Title,Type,Platform,Author))
+res
+
+df = pd.DataFrame (res, columns= ['Date','D','A','V','Title','Type', 'Platform', 'Author'])
+df
+
+browser.close()
